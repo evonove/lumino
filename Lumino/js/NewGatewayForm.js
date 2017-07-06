192f6ae0f2905ded38b0827655365014c813194f
Alessio Rapini <alessio.rapini@yahoo.it>
2017-07-06 15:48:48 +0200

    Layout and style the gateway form screen

--- Lumino/js/NewGatewayForm.js
+++ Lumino/js/NewGatewayForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 /**
  * Input form component for configuring a gateway.
@@ -22,29 +22,52 @@ export default class NewGatewayForm extends React.Component {
   }
 
   render() {
+    // Form block headings
+    const headings = {
+      info: 'info',
+      settings: 'settings',
+    }
+
     return (
       <View style={styles.container}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Name"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <TextInput
-          style={styles.textInput}
-          placeholder="IP Address"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <TextInput
-          style={styles.textInput}
-          placeholder="Port"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <TextInput
-          style={styles.textInput}
-          placeholder="Password"
-          secureTextEntry={true}
-          onChangeText={(text) => this.setState({text})}
-        />
+        <View>
+          <View style={styles.blockHeading}>
+            <Text style={styles.textHeading}>{headings.info.toUpperCase()}</Text>
+          </View>
+          <View style={styles.blockFields}>
+            <TextInput
+              style={styles.textInput}
+              tintColor="red"
+              placeholder="Name"
+              onChangeText={(text) => this.setState({text})}
+            />
+          </View>
+        </View>
+        <View>
+          <View style={styles.blockHeading}>
+            <Text style={styles.textHeading}>{headings.settings.toUpperCase()}</Text>
+          </View>
+          <View style={styles.blockFields}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="IP Address"
+              onChangeText={(text) => this.setState({text})}
+            />
+            <View style={styles.fieldDivider}></View>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Port"
+              onChangeText={(text) => this.setState({text})}
+            />
+            <View style={styles.fieldDivider}></View>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Password"
+              secureTextEntry={true}
+              onChangeText={(text) => this.setState({text})}
+            />
+          </View>
+        </View>
       </View>
     )
   }
@@ -56,9 +79,28 @@ export default class NewGatewayForm extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  blockHeading: {
+    paddingBottom: 8,
     paddingHorizontal: 20,
+    paddingTop: 18,
+  },
+  textHeading: {
+    color: 'grey',
+  },
+  blockFields: {
+    backgroundColor: 'white',
+    borderBottomWidth: 0.5,
+    borderColor: 'lightgrey',
+    borderTopWidth: 0.5,
   },
   textInput: {
-    height: 40,
-  }
+    height: 46,
+    paddingHorizontal: 20,
+  },
+  fieldDivider: {
+    backgroundColor: 'lightgrey',
+    height: 0.5,
+    marginLeft: 20,
+  },
 });
