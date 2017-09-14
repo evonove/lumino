211ccec2311d3becf2c04f9c746f57b4ce6c41eb
Federico Dolce <psykopear@gmail.com>
2017-09-14 12:27:03 +0200

    Improved state management

--- Lumino/js/openwebnet/index.js
+++ Lumino/js/openwebnet/index.js
@@ -1,33 +1,43 @@
 import net from 'net';
 
-export const change_light = (gateway, controller) => {
-  let value = 0;
-  if (controller.value === true) {
-    value = 1;
-  } else if (controller.value === false) {
-    value = 0;
-  } else {
-    value = controller.value;
-  }
-  const command_string = '*' + controller.zone_code + '*' + value + '*' + controller.id_code + '##'
-  let client = net.createConnection(gateway.port, gateway.ip_address, () => {
-    client.write(command_string);
+
+// Method called on network errors
+const onError = (err) => console.warn(err);
+
+// Method to build a 'Command/Status Message'
+const commandStatusMessage = (zone_code, id_code, value) => `*${zone_code}*${value}*${id_code}##`;
+
+// Method to build a 'Request/Read/Write Dimension Message'
+const requestMessage = (zone_code, id_code, dimension) => `*#${zone_code}*${id_code}*${dimension}##`;
+
+// Converts an ACK message into a boolean
+const isOn = (ack) => ack.toString() === '*#*1##' ? true : false;
+
+
+export const changeLight = (gateway, controller) => {
+  // Controller could send us a boolean, in which case
+  // we transform it into either 1 or 0
+  const { zone_code, id_code, value } = controller;
+  const val = value === true ? 1 : value === false ? 0 : value;
+
+  const { ip_address, port } = gateway;
+  let client = net.connect(port, ip_address, () => {
+    // Calling end with data is the same as calling client.write(data)
+    // first and client.end() just after
+    client.write(commandStatusMessage(zone_code, id_code, val));
     client.end();
   });
+  client.on('error', onError);
 }
 
-export const read_light = (gateway, controller, callback) => {
-  const command_string = '*#' +
-    controller.zone_code +
-    '*' +
-    controller.id_code +
-    '*0##';
 
-  let client = net.createConnection(gateway.port, gateway.ip_address, () => {
-    client.write(command_string);
+export const readLight = (gateway, controller, callback) => {
+  const { ip_address, port } = gateway;
+  let client = net.connect(port, ip_address, () => {
+    client.write(requestMessage(zone_code, id_code, 0));
   });
 
   client.on('data', (data) => {
-    callback(parseInt(data.toString().replace(/\D/g,'')));
+    callback(isOn(data));
   });
 }
--- Lumino/js/reducers/controllers.js
+++ Lumino/js/reducers/controllers.js
@@ -1,56 +1,50 @@
 import uuid from 'react-native-uuid';
 
-import { change_light, read_light } from '../openwebnet';
+import { changeLight, readLight } from '../openwebnet';
 
+
+// Helper method to extract gateway object from the global state
+// c stands for Controller
+const getGateway = (c, getState) => getState().gateways.filter((g) => g.id === c.gateway)[0];
 
 // Contollers reducer
 const controllers = (state = [], action) => {
-
-  let values = {};
-  let gateways = [];
-  let newState = [];
-  const getGateway = (c, gateways) => gateways.filter((g) => g.id === c.gateway)[0];
-
   switch (action.type) {
 
     case 'ADD_CONTROLLER':
-      gateways = action.getState().gateways;
-      values = action.getState().form.controller.values;
-      // Take form values from global state as we won't have that
-      // in the navigationOptions (where the button is)
-      values.id = uuid.v4();
-      values.gateway_name = getGateway(values, gateways).name;
-      return [...state, values];
+      const newController = {
+        ...action.values,
+        id: uuid.v4(),
+        gateway_name: getGateway(action.values, action.getState).name
+      }
+      return [...state, newController];
 
     case 'EDIT_CONTROLLER':
-      gateways = action.getState().gateways;
-      values = action.getState().form.controller.values;
-      // Remove old controller value and add new ones from form
-      values.gateway_name = getGateway(values, gateways).name;
-      let filteredState = state.filter((g) => g.id != values.id);
-      return [...filteredState, values];
+      const filteredState = state.filter((g) => g.id != values.id);
+
+      const editedController = {
+        ...action.values,
+        gateway_name: getGateway(action.values, action.getState).name
+      }
+      return [...filteredState, editedController];
 
     case 'DELETE_CONTROLLER':
       return state.filter((c) => c.id !== action.controller);
 
     case 'READ_CONTROLLERS':
-      console.warn("READ!");
       // Get active gateways
-      gateways = action.getState().gateways.filter((g) => g.status);
-      state.forEach((c) => read_light(getGateway(c, gateways), c, (val) => console.warn(val)));
-
-      // Read value from gateway and set it into the state
+      // state.forEach((c) => readLight(getGateway(c, action.getState), c, (val) => console.warn(val)));
+      console.warn("READ!");
       return state
 
     case 'CHANGE_CONTROLLER':
-      gateways = action.getState().gateways;
       // Create new state with a fixed controller value
       // Also send the command
       return state.map((c) => {
         if (c.id === action.id) {
           // Update toggle value
           c.value = action.value;
-          change_light(getGateway(c, gateways), c);
+          changeLight(getGateway(c, action.getState), c);
         };
         return c;
       });
